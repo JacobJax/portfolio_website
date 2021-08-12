d257db55006d51a9495ee92b5f5869995a59f9f0
Jacob Odero <jackjax617@gmail.com>
2021-08-12 21:03:41 +0300

    change fonts and make navbar responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: "'Roboto', sans-serif",
+        roboto_mono: "'Roboto Mono', monospace",
+        space_g: "'Space Grotesk', sans-serif",
+        space_mono: "'Space Mono', monospace"
+      },
+    },
   },
   variants: {
     extend: {},
