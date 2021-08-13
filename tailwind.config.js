9ecaba87b94a461170bf5e08471b8b3071ea8512
Jacob Odero <jackjax617@gmail.com>
2021-08-13 11:47:24 +0300

    add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,23 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      backgroundColor: {
+        fblue: "#121434"
+      },
+      textColor: {
+        fblue: "#121434"
+      },
       fontFamily: {
         roboto: "'Roboto', sans-serif",
         roboto_mono: "'Roboto Mono', monospace",
         space_g: "'Space Grotesk', sans-serif",
         space_mono: "'Space Mono', monospace"
       },
+      height: {
+        h50: "50vh",
+        h80: "80vh",
+        h90 : "95vh"
+      }
     },
   },
   variants: {
